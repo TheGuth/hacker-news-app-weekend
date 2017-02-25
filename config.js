c692eca10b3a19a15edd9223f6890c9b33a5658c
James Guthrie <jamesrguthrie@gmail.com>
2017-02-25 17:41:21 -0500

    added initial files, and configured server.js and config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const DATABASE_URL = process.env.DATABASE_URL ||
+                     global.DATABASE_URL ||
+                     'mongodb://localhost/hn-api';
+
+const PORT = process.env.PORT || 8080;
+
+module.exports = {DATABASE_URL, PORT};
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const {DATABASE_URL, PORT} = require('./config');
+
+mongoose.Promise = global.Promise;
+
+
+const app = express();
+app.use(bodyParser.json());
+
+// API endpoints go here
+
+let server;
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(PORT, () => {
+        console.log(`Your app is listening on port ${PORT}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
